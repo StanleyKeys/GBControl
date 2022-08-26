efa0680a37cefecf31817df6f0a427066dc56523
StanleyGunz <stanislavkinsler@gmail.com>
2022-08-26 15:30:26 +0700

    added code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.Clear();
+Console.Write("Input your text: ");
+
+string userEnter = Console.ReadLine();
+
+string[] subs = userEnter.Split();
+
+Console.WriteLine($"\nв строке слова, у которых меньше 3 символов: ");
+
+for (int i = 0; i < subs.Length; i++)
+{
+    string temp = subs[i];
+        if (temp.Length <= 3)
+    {
+        Console.Write(temp + " ");
+    }
+}
+
